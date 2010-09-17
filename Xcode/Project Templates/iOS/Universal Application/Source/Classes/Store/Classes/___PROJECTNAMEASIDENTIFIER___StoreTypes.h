591802a369dcefe92f078b9969d37381ab402161
Dave Morford <david.morford@me.com>
2010-09-17 08:22:56 -0700

    Moving/renaming project templates

--- Xcode/Project Templates/iOS/Universal Application/Source/Classes/Store/Classes/___PROJECTNAMEASIDENTIFIER___StoreTypes.h
+++
@@ -1,4 +0,0 @@
-
-#import <Foundation/Foundation.h>
-
-extern const NSString *___PROJECTNAMEASIDENTIFIER___StateEntityName
--- Xcode/Project Templates/iOS/Universal Application/Source/Classes/Store/Classes/___PROJECTNAMEASIDENTIFIER___StoreTypes.m
+++
@@ -1,5 +0,0 @@
-
-#import "___PROJECTNAMEASIDENTIFIER___StoreTypes.h"
-
-const NSString *___PROJECTNAMEASIDENTIFIER___StateEntityName; = @"___PROJECTNAMEASIDENTIFIER___State";
-
