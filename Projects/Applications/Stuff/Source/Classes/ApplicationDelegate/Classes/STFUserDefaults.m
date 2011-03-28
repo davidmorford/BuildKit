a0c7824a05a815e708a4b474125c1c7e72db1281
David Morford <david.morford@me.com>
2011-03-28 01:22:50 -0700

    Remove refactored files.

--- Projects/Applications/Stuff/Source/Classes/ApplicationDelegate/Classes/STFUserDefaults.m
+++
@@ -1,8 +0,0 @@
-
-#import "STFUserDefaults.h"
-
-#pragma mark Application Preference Keys
-
-NSString * const STFApplicationVersionKey			= @"STFApplicationVersionKey";
-NSString * const STFApplicationBuildNumberKey		= @"STFApplicationBuildNumberKey";
-NSString * const STFApplicationResetKey			= @"STFApplicationResetKey";
